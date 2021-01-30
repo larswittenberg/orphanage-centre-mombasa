5311106a343215543010a0d657d6431f48252292
Lars Wittenberg <larswittenberg@googlemail.com>
2021-01-30 18:11:30 +0100

    UPDATE: Polaroid Component

--- components/Polaroid.js
+++ components/Polaroid.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import NextImage from 'next/image'
 
 export default function Polaroid({ filename, alt, w, h, rotationRight, className }) {
 	return (
 		<div className={`shadow-polaroid p-3 pb-12 ${rotationRight ? 'transform rotate-1' : 'transform -rotate-1'} ${className} `}>
-			<Image
+			<NextImage
 				src={`/images/${filename}`}
 				alt={alt}
 				width={w}
