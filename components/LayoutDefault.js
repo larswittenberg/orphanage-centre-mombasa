9e4dfe9f438ce7a09e80cd2b6b7ffaccd3fdba48
Lars Wittenberg <larswittenberg@googlemail.com>
2021-02-01 21:31:39 +0100

    FEATURE: Add Homepage Teaser

--- components/LayoutDefault.js
+++ components/LayoutDefault.js
@@ -2,11 +2,13 @@ import { MDXProvider } from '@mdx-js/react'
 import MDXComponents from './MDXComponents';
 import Nav from './Nav'
 import Footer from './Footer'
+import Teaser from './Teaser'
 
-export default function LayoutDefault({ children }) {
+export default function LayoutDefault({ children, withTeaser }) {
 	return (
 		<div>
 			<Nav />
+				{withTeaser ? <Teaser /> : ''}
 				<MDXProvider components={MDXComponents}>
 					<main className="px-4 prose prose-sm sm:prose lg:prose-xl mx-auto">
 						{children}
---
+++ components/Teaser.js
@@ -0,0 +1,21 @@
+import NextImage from 'next/image'
+
+export default function Teaser(){
+	return (
+		<div className="relative mb-4 lg:mb-16">
+			<NextImage
+				src="/images/comoc.jpg"
+				alt="Child of Mercy Orphanage Centre"
+				width="1920"
+				height="1075"
+			/>
+			<h1 className="absolute top-0 left-0 right-0 text-xl md:text-4xl lg:text-6xl font-light text-center text-white flex flex-col items-center mt-4 md:mt-8 lg:mt-12">
+				<span className="bg-gray-900 bg-opacity-80 py-1 px-2 md:py-2 md:px-6 lg:py-3 lg:px-8 mb-1 md:mb-3 lg:mb-4">A wholehearted welcome to the</span>
+				<span className="bg-gray-900 bg-opacity-80 py-1 px-2 md:py-2 md:px-6 lg:py-3 lg:px-8">Child of Mercy Orphanage Centre</span>
+			</h1>
+		</div>
+	)
+}
+
+
+
