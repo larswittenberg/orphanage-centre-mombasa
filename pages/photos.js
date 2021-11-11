a1a92c80650b6506f2e6828f9e21d23c702b8919
Lars Wittenberg <larswittenberg@googlemail.com>
2021-11-11 01:39:31 +0100

    UPDATE: Photo Gallery with NextImages

--- pages/photos.js
+++ pages/photos.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
 import path from 'path'
+import sizeOf from 'image-size'
+import Image from 'next/image'
 import Navigation from '../components/Navigation'
 import Footer from '../components/Footer'
 
@@ -11,10 +13,16 @@ function Photos({ photos }) {
 					<div className="prose sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl mx-auto px-4 pt-4 md:pt-16">
 						<h1 className="">Photo-Gallery</h1>
 					</div>
-					<ul className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-2 max-w-screen-xl mx-auto mt-24 px-4">
+					<ul className="gallery grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-2 max-w-screen-xl mx-auto mt-24 px-4">
 						{photos.map((photo, i) => (
-							<li key={i} className="block h-40 sm:h-48 md:h-56 lg:h-64">
-								<img src={`/images/photogallery/${photo}`} className="block w-full h-full object-cover bg-gray-100" />
+							<li key={i} className="block overflow-hidden h-40 sm:h-48 md:h-56 lg:h-64">
+								<Image
+									src={`/images/photogallery/${photo.fileName}`}
+									width={photo.fileDimensions.width}
+									height={photo.fileDimensions.height}
+									quality={70}
+									className="block w-full h-full object-cover bg-gray-100"
+								/>
 							</li>
 						))}
 					</ul>
@@ -30,7 +38,17 @@ function Photos({ photos }) {
 // It won't be called on client-side.
 export async function getStaticProps() {
 	const photosDirectory = path.join(process.cwd(), '/public/images/photogallery')
-	const photos = fs.readdirSync(photosDirectory)
+	const photoFiles = fs.readdirSync(photosDirectory)
+
+	const photos = photoFiles.map(fileName => {
+		const filePath = path.join(photosDirectory, fileName)
+		const fileDimensions = sizeOf(filePath)
+
+		return {
+			fileName,
+			fileDimensions,
+		}
+	})
 
 	// By returning { props: photos }, the Photos component
 	// will receive `photos` as a prop at build time
