b0339d4c4aeac2c6281eca2160b65456abae4be7
Lars Wittenberg <larswittenberg@googlemail.com>
2021-11-11 01:07:33 +0100

    FIX: Photo Gallery

--- pages/photos.js
+++ pages/photos.js
@@ -11,9 +11,9 @@ function Photos({ photos }) {
 					<div className="prose sm:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl mx-auto px-4 pt-4 md:pt-16">
 						<h1 className="">Photo-Gallery</h1>
 					</div>
-					<ul className="grid grid-cols-4 gap-2 max-w-screen-xl mx-auto mt-24">
+					<ul className="grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-2 max-w-screen-xl mx-auto mt-24 px-4">
 						{photos.map((photo, i) => (
-							<li key={i} className="block h-72">
+							<li key={i} className="block h-40 sm:h-48 md:h-56 lg:h-64">
 								<img src={`/images/photogallery/${photo}`} className="block w-full h-full object-cover bg-gray-100" />
 							</li>
 						))}
