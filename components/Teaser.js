a3e1e37d6948017029e5439779c031dddd151485
Lars Wittenberg <larswittenberg@googlemail.com>
2022-01-06 15:18:40 +0100

    UPDATE: Teaser Image Priority

--- components/Teaser.js
+++ components/Teaser.js
@@ -9,6 +9,7 @@ export default function Teaser(){
 				width="1920"
 				height="1075"
 				layout="responsive"
+				priority
 			/>
 			<h1 className="absolute top-0 left-0 right-0 text-xl md:text-4xl lg:text-6xl font-light text-center text-white flex flex-col items-center mt-4 md:mt-8 lg:mt-12">
 				<span className="bg-gray-900 bg-opacity-80 py-1 px-2 md:py-2 md:px-6 lg:py-3 lg:px-8 mb-1 md:mb-3 lg:mb-4">A wholehearted welcome to the</span>
